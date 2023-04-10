fdc1c24cceec825659213f61bb34e672ee6e6a9d
davidwoong2 <127732977+davidwoong2@users.noreply.github.com>
2023-04-10 18:39:46 +0900

    Update 3.ino

--- 3.ino
+++ 3.ino
@@ -13,6 +13,8 @@ void loop() {
   
   
   delay(1000);                       // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(2, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(3, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(4, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                    // wait for a second
 }
